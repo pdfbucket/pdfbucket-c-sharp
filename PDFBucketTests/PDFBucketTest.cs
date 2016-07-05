0be738ec70e45e1e96b68d298a9c598dc5096b64
Gustavo A. López <gualopezb@gmail.com>
2016-07-05 17:38:39 -0500

    Add skeleton to tests

---
+++ PDFBucketTests/PDFBucketTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PdfBucket;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PDFBucketTests
+{
+    [TestClass]
+    public class PDFBucketTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
